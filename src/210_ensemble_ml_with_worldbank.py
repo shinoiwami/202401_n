2caf7dca00e6a4e12d446d340d472fe193a75cb8
shinoiwami <shino.iwami@gmail.com>
2024-08-14 23:53:57 +0900

    Update: The types of output data have been increased.

--- src/210_ensemble_ml_with_worldbank.py
+++ src/210_ensemble_ml_with_worldbank.py
@@ -5,6 +5,7 @@
 import os
 import re
 import sys
+import time
 import json
 import zipfile
 
@@ -26,7 +27,8 @@ output_file = output_dir+"/_importances.json"
 
 tmp_dir = "./tmp"
 
-gdp_index = ["NY.GDP.MKTP.CD", "NY.GDP.MKTP.KD.ZG", "NY.GDP.PCAP.CD"]
+#gdp_index = ["NY.GDP.MKTP.CD", "NY.GDP.MKTP.KD.ZG", "NY.GDP.PCAP.CD"]
+gdp_index = ["NY.GDP.MKTP.KD.ZG", "NY.GDP.PCAP.CD"]
 #gdp_index = ["NY.GDP.PCAP.CD"]
 
 removed_factor_index = ["NY.GDP.MKTP.KD", "NY.GDP.MKTP.KN", "NY.GDP.MKTP.CN", "NY.GDP.MKTP.CD", "NY.GDP.DEFL.ZS", "NY.GDP.MKTP.KD.ZG", "NY.GDP.PCAP.KD", "NY.GDP.PCAP.KN", "NY.GDP.PCAP.CN", "NY.GDP.PCAP.CD", "NY.GDP.PCAP.KD.ZG", "NY.GDP.PCAP.PP.KD", "NY.GDP.PCAP.PP.CD", "NY.GDP.MKTP.PP.KD", "NY.GDP.MKTP.PP.CD", "NY.GNP.MKTP.CN.AD"]	# GDP-related
@@ -58,11 +60,18 @@ if len(test_index) > 0:
 	factor_index = test_index
 
 ###
-# GOAL: output[destination_index][country][source_index]["year"] = gap_year
-#       output[destination_index][country][source_index]["importance"] = importance
+# GOAL: output[destination_index][country][source_index]["importance"] = importance
 #			destination_index: GDP-related
 #			source_index: out of GDP
 #			gap_year: GDP_year - source_index_year
+#		other keys (st position of "importance")
+#			"importance": the largest importance during gap_period
+#			"r2": coefficient of determination
+#			"cor": correlation coefficient
+#			"x_year_0", "x_year_1": period of source
+#			"y_year_0", "y_year_1": period of destination
+###
+
 output = {}
 country_index = []
 country_name = {}
@@ -141,19 +150,19 @@ for dst in gdp_index:
 							year1 = year_max
 
 							# check period
-							nan = 0
+							flag = 0
 							for yr in reversed(range(year_min, year_max+1)):
-								if nan == 0 and not pd.isnull(data0[str(yr)][country_num[cc]]) and not pd.isnull(data1[str(yr)][country_num[cc]]):
+								if flag == 0 and not pd.isnull(data0[str(yr)][country_num[cc]]) and not pd.isnull(data1[str(yr)][country_num[cc]]):
 									year1 = yr
-									nan = 1
-								elif nan == 1 and (pd.isnull(data0[str(yr)][country_num[cc]]) or pd.isnull(data1[str(yr)][country_num[cc]])):
+									flag = 1
+								elif flag == 1 and (pd.isnull(data0[str(yr)][country_num[cc]]) or pd.isnull(data1[str(yr)][country_num[cc]])):
 									year0 = yr+1
-									nan = 2
+									flag = 2
 									break
 
 #							print("=====")
 #							print(cc, year1, year0, gap_period, year1-year0, gap_period+1)
-							if nan == 0 or year1-year0 <= gap_period+1:
+							if flag == 0 or year1-year0 <= gap_period+1:
 								continue
 
 							for gap in range(gap_period):
@@ -177,24 +186,38 @@ for dst in gdp_index:
 							clf = ExtraTreesClassifier(n_estimators=100, random_state=0)
 
 							try:
-								clf.fit(Xnp,ynp)
-								r2 = clf.score(Xnp,ynp)
+								clf.fit(Xnp, ynp)
+								r2 = clf.score(Xnp, ynp)
 								importances = clf.feature_importances_
-#								print("Success:", importances, r2)
+								corr = np.corrcoef(y, X)
+#								print("Success:", importances, r2, corr)
 
 								i = 0
-								importance_score = 0
+								importance_val = 0
 								importance_gap = 0
 								for im in importances:
-									if importance_score < im:
-										importance_score = im
+									if importance_val < im:
+										importance_val = im
 										importance_gap = i
 									i += 1
-								output[dst][cc].setdefault(src, {})
-								output[dst][cc][src]["year"] = year1 - importance_gap
-								output[dst][cc][src]["importance"] = importance_score
 
-							
+								output[dst][cc].setdefault(src, {})
+#								output[dst][cc][src]["year"] = year1 - importance_gap
+								output[dst][cc][src]["importance"] = importance_val
+								output[dst][cc][src]["r2"] = r2
+								if str(corr[0][importance_gap+1]) != 'nan':
+									output[dst][cc][src]["cor"] = corr[0][importance_gap+1]
+								else:
+									output[dst][cc][src]["cor"] = 0
+								output[dst][cc][src]["x_year_0"] = year0+gap_period-importance_gap
+								output[dst][cc][src]["x_year_1"] = year1-importance_gap
+								output[dst][cc][src]["y_year_0"] = year0+gap_period
+								output[dst][cc][src]["y_year_1"] = year1
+								print("Identified Values:", dst, "["+cc+"]", src, importance_gap, importance_val, r2, output[dst][cc][src]["cor"])
+
+								# sleep to reduce CPU burden slightly (Please tune depending on your computer.)
+#								time.sleep(0.1)
+
 							except ValueError as e:
 #								print("type:{0}".format(type(e)))
 #								print("args:{0}".format(e.args))
@@ -204,10 +227,14 @@ for dst in gdp_index:
 
 						break
 
-
+ 
 	###
 	# output
 	fw = open(output_file, 'w', encoding='UTF-8')
+	fw.write(json.dumps(output)+"\n")
+	fw.close()
+
+	fw = open(output_file+"."+dst, 'w', encoding='UTF-8')
 	fw.write(json.dumps(output)+"\n")
 	fw.close()
 
--- src/230_top_importance_with_worldbank.py
+++ src/230_top_importance_with_worldbank.py
@@ -13,11 +13,12 @@ from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 
 countries_file = "./download/_countries.json"
 indicators_file = "./download/_indicators.json"
-input_file = "./output/_importances.json"
+#input_file = "./output/_importances.json"
+input_file = "./output/_importances.json.NY.GDP.MKTP.CD"
 output_dir = "./output"
 
 top = 20
-target_country_codes = ["USA", "JPN", "DEU", "IND", "SAU", "THA", "PRY", "HUN", "NZL", "SGP", "GUY", "EST"]
+target_country_codes = ["USA", "JPN", "DEU", "SAU", "GUY", "EST"]
 
 
 
@@ -37,12 +38,17 @@ with open(input_file, 'rb') as infile:
 #	print(importances)
 
 
-# Data Format:
-#		importances[destination_index][country][source_index]["year"] = gap_year
-#		importances[destination_index][country][source_index]["importance"] = importance
-#		 	destination_index: GDP-related
+###
+# GOAL: output[destination_index][country][source_index]["importance"] = importance
+#			destination_index: GDP-related
 #			source_index: out of GDP
 #			gap_year: GDP_year - source_index_year
+#		other keys (st position of "importance")
+#			"importance": the largest importance during gap_period
+#			"r2": coefficient of determination
+#			"cor": correlation coefficient
+#			"x_year_0", "x_year_1": period of source
+#			"y_year_0", "y_year_1": period of destination
 ###
 
 for dst in importances.keys():
@@ -62,19 +68,24 @@ for dst in importances.keys():
 		order = {}
 		year = {}
 		for src in src_list:
-			importances[dst][cc].setdefault(src, {"year": 0, "importance": 0})
+			importances[dst][cc].setdefault(src, {"importance": 0})
 			order[src] = importances[dst][cc][src]["importance"]
-			year[src] = importances[dst][cc][src]["year"]
 
 		i = 0
 		output_path = output_dir+"/"+dst+"_"+cc+"_top"+str(top)+".tsv"
 		outfile = open(output_path, 'w', encoding='utf-8')
+		outfile.write("Rank\tImportance\tIndicator ID\tIndicator Name\tR2\tCOR\tX Period\tY Period\n")
 		for k, v in sorted(order.items(), key=lambda i: i[1], reverse=True):
 			i += 1
 			if v != 0:
-				outfile.write(str(i)+"\t"+str(v)+"\t"+k+"\t"+indicators[k]+"\t"+str(year[k])+"\n")
+				text = str(i)+"\t"+str(round(v,4))+"\t"+k+"\t"+indicators[k]
+				text += "\t"+str(round(importances[dst][cc][k]["r2"],4))
+				text += "\t"+str(round(importances[dst][cc][k]["cor"],4))
+				text += "\t"+str(importances[dst][cc][k]["x_year_0"])+" - "+str(importances[dst][cc][k]["x_year_1"])
+				text += "\t"+str(importances[dst][cc][k]["y_year_0"])+" - "+str(importances[dst][cc][k]["y_year_1"])
+				outfile.write(text+"\n")
 			else:
-				outfile.write(str(i)+"\t-\t-\t-\t-\n")
+				outfile.write(str(i)+"\t-\t-\t-\t-\t-\t-\t-\n")
 			if i >= top:
 				break
 		outfile.close()
